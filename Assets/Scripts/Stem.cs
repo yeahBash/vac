0cb3b555bf560ad48510c24b95bba19f5711a2e9
Ilnur_Ishkaev <ilnurishkaev@yandex.ru>
2021-08-14 22:17:57 +0300

    add blade and flower prefabs; basic blade animation

---
+++ Assets/Scripts/Stem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CutTheFlowers
+{
+    public class Stem : MonoBehaviour
+    {
+        private void Start()
+        {
+
+        }
+    }
+}
